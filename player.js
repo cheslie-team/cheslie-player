b00e7a693652b382770471ac5ec9843eacbbfff3
Teodor Ande Elstad <teodor@andeelstad.com>
2017-03-19 00:47:09 +0100

    Add move handler and send random move to game

--- player.js
+++ player.js
@@ -1,5 +1,19 @@
-var socket = require('socket.io-client')('http://localhost:3000');
+var socket = require('socket.io-client')('http://192.168.0.24:3000'),
+    Chess = require('chess.js').Chess;
 
 socket.on('connect', function () {
     console.log('Player is connected');
+});
+
+socket.on('move', function (game) {
+    var chess = new Chess();
+    chess.load(game.board);
+ 
+    var moves = chess.moves(),
+        move = moves[Math.floor(Math.random() * moves.length)];
+
+    console.log(move); 
+    game.move = move;
+
+    socket.emit('move', game);
 });
