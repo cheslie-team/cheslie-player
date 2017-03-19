abb6e5d814ab831be95b736c830d4bb6ec9b3edc
Teodor Ande Elstad <teodor@andeelstad.com>
2017-03-19 01:48:17 +0100

    Add name to player

--- player.js
+++ player.js
@@ -1,9 +1,11 @@
 var game = require('socket.io-client')('http://192.168.0.24:3000'),
     lobby = require('socket.io-client')('http://192.168.0.24:3030'),
-    Chess = require('chess.js').Chess;
+    Chess = require('chess.js').Chess,
 
-game.on('connect', function () {
-    console.log('Player is connected to game');
+    name = 'RndJesus_' + Math.floor(Math.random() * 100);;
+
+lobby.on('connect', function () {
+    console.log('Player ' + name + ' is connected to lobby');
 });
 
 lobby.on('join', function (gameId) {
@@ -11,15 +13,20 @@ lobby.on('join', function (gameId) {
     game.emit('join', gameId);
 });
 
-game.on('move', function (game) {
+game.on('connect', function () {
+    console.log('Player ' + name + ' is connected to game');
+    //game.emit('join', 'test_game');
+});
+
+game.on('move', function (gameState) {
     var chess = new Chess();
-    chess.load(game.board);
+    chess.load(gameState.board);
  
     var moves = chess.moves(),
         move = moves[Math.floor(Math.random() * moves.length)];
 
     console.log(move);
-    game.move = move;
+    gameState.move = move;
 
-    game.emit('move', game);
+    game.emit('move', gameState);
 });
