1eed96f126dcb2b57b44b194ec6bb7ac99456339
Teodor Ande Elstad <teodor@andeelstad.com>
2017-03-22 00:34:03 +0100

    Add minmax implementation and example of use in ai.js

--- ai.js
+++ ai.js
@@ -1,5 +1,15 @@
 var Chess = require('chess.js').Chess;
 
+/* MinMax player example. Commented out since it runs slowly
+var minmax = require('./minmax.js').minmax;
+
+exports.bestMove = function (board) {
+    return minmax(board, function (board) {
+    	return 0; // a number between Number.NEGATIVE_INFINITY and Number.POSITIVE_INFINITY
+    })[0].move;
+};
+*/
+
 exports.bestMove = function (board) {
     var chess = new Chess(board);
     var moves = chess.moves()
