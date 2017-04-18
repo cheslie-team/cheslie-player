9b3f5d429f507296e4e38e0b7641e001c1e55052
teodoran <teodor@andeelstad.com>
2017-04-18 02:39:48 +0200

    Describing use of chess.js in minmax

--- sample-players/minmaxer.js
+++ sample-players/minmaxer.js
@@ -2,7 +2,8 @@ var Chess = require('../modules/chess-extended.js').Chess,
 	minmax = require('../modules/minmax.js');
 
 exports.move = function (board, doMove) {
-    minmax.move(board, doMove, function (board) {
+    minmax.move(board, doMove, function (chess) {
+        // chess is a chess.js instance
     	// A number between Number.NEGATIVE_INFINITY and Number.POSITIVE_INFINITY
     	return Math.random() * 200 - 100;
     }, 2);
