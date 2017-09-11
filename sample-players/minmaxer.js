81962ef1b871c0823ba543fd8dd268d28b4e9377
teodoran <teodor@andeelstad.com>
2017-09-11 04:10:18 +0200

    Make minmaxer more interesting

--- sample-players/minmaxer.js
+++ sample-players/minmaxer.js
@@ -3,11 +3,10 @@ var Chess = require('../modules/chess-extended.js').Chess,
 
 exports.move = function (board) {
     var depth = 2,
-        score = function (chess) {
-            // chess is a chess.js instance
-            // A number between Number.NEGATIVE_INFINITY and Number.POSITIVE_INFINITY
-            return Math.random() * 200 - 100;
-        };
+		score = function (chess) { // chess is a chess.js instance
+			var playingAsColor = chess.turn(); // Is your player playing as white or black?
+			return chess.numberOfPieces(playingAsColor); // Return a number between Number.NEGATIVE_INFINITY and Number.POSITIVE_INFINITY
+		};
 
     return minmax.move(board, depth, score);
 };
